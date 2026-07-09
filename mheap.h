079dcbddd7b1caf8ac22e7fac417073913bcd0e3
maxcorces <max.corces@ufl.edu>
2026-07-08 20:31:04 -0400

    mheap h

---
+++ mheap.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <string>
+#include <vector>
+using namespace std;
+
+class MaxHeap {
+    struct Node {
+        int movie_id;
+        string movie_name;
+        int movie_score;
+    };
+
+    vector<Node> heap;
+
+    int parent(int index) const;
+    int leftChild(int index) const;
+    int rightChild(int index) const;
+
+    bool higherPriority(const Node& a, const Node& b) const;
+    void heapifyUp(int index);
+    void heapifyDown(int index);
+    Node extractMax();
+
+public:
+    MaxHeap();
+
+    void insert(int movie_id, string movie_name, int movie_score);
+    void top_n(int n);
+
+    bool empty() const;
+    int size() const;
+};
